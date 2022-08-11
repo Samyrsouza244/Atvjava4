c4e87d9727b895ace8924b2f1efc3ea7edda93ee
Samyrsouza244 <samyr1479ee@gmail.com>
2022-08-11 14:31:57 -0300

    entegue

---
+++ script.js
@@ -0,0 +1,29 @@
+
+function Mensagem() {
+    console.log('uma mensagem no console');
+  }
+  Mensagem();
+  
+  function Name(nome) {
+    console.log(nome);
+  }
+  Name('Samyr');
+   
+  function Dados(nome, idade, musica) {
+    console.log(nome, idade, musica);
+  }
+  Dados('meu nome é Samyr');
+  Dados('tenho 17 anos');
+  Dados('meus estilo musica preferido são samba e rock');
+   
+  function Information(filmes, musica) {
+    console.log(filmes, musica);
+  }
+  Information('Meu filme favorito é Harry potter');
+  Information('minha musica favorita é Yoko');
+  
+  function triple(number) {
+    return number * 3;
+  }
+  triple(9);
+  console.log(triple(9));
